c644428f661f3cdcb021b540b8ba2630d89beff7
Eugenia Minnah <eaminnah@st.ug.edu.gh>
2024-05-27 13:57:11 +0000

    changed the fontweight

--- myApp/App.js
+++ myApp/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>My name is Eugenia</Text>
+      <Text style={styles.text}>My name is <Text style={styles.textBold}>Eugenia</Text></Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -19,6 +19,9 @@ const styles = StyleSheet.create({
   },
   text:{
     fontSize: 24,
+  },
+  textBold:{
+    fontWeight: 'bold',
   },
  
 });
