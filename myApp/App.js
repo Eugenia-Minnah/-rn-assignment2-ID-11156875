21e1d825d4e23619dcaee4ee7c56fcb42b6564c1
Eugenia Minnah <eaminnah@st.ug.edu.gh>
2024-05-27 13:55:06 +0000

    changed the font size

--- myApp/App.js
+++ myApp/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>My name is Eugenia</Text>
+      <Text style={styles.text}>My name is Eugenia</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,6 +16,9 @@ const styles = StyleSheet.create({
     backgroundColor: 'green',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text:{
+    fontSize: 24,
   },
  
 });
